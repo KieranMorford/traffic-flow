43707ddecdf46e17c2136788a44444b8857a300e
KieranMorford <53496068+KieranMorford@users.noreply.github.com>
2025-12-06 15:00:05 -0700

    Set structure

--- TrafficFlow.m
+++ TrafficFlow.m
@@ -2,3 +2,39 @@
 
 clear;
 close all;
+
+%% ROUNDABOUT
+
+% parameters
+
+% PDE
+
+% Solve PDEs
+
+% Unload Solutions
+
+% Plot Density vs Flux
+
+% Plot Phase Diagram
+
+%% FOUR WAY STOP
+
+% parameters
+
+% PDE
+
+% Solve PDEs
+
+% Unload Solutions
+
+% Plot Density vs Flux
+
+% Plot Phase Diagram
+
+%% COMPARISON
+
+% Plot Density vs Flux Comparison
+
+% Plot Phase Diagram Comparison
+
+% Comparison Conclusion
